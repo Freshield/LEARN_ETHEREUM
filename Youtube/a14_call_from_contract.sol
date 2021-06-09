ee665b527a7e408f3488339612874e4a6478bb02
Freshield <yangyufresh@163.com>
2021-06-09 16:39:07 +0800

    done call from contract

---
+++ Youtube/a14_call_from_contract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract ContractA {
+    uint public x;
+    uint public value;
+    
+    function setX(uint _x) public returns (uint) {
+        x = _x;
+        return x;
+    }
+    
+    function setXandSendEther(uint _x) public payable returns (uint ,uint) {
+        x = _x;
+        value = msg.value;
+        return (x, value);
+    }
+    
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+}
+
+contract ContractB {
+    
+    function callSetX(ContractA _contractA, uint _x) public {
+        _contractA.setX(_x);
+    }
+    
+    function callSetXFromAddress(ContractA _contractA, uint _x) public payable {
+        _contractA.setXandSendEther{value: msg.value}(_x);
+    }
+    
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
